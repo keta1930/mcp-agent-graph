590cb3d5fa770844be50f0027cd06f13960bf45c
keta1930 <421858037@qq.com>
2025-11-28 11:55:31 +0800

    feat(tour): export tour components and hooks from index module

---
+++ frontend/src/components/tour/index.ts
@@ -0,0 +1,5 @@
+// src/components/tour/index.ts
+export { useTour } from './useTour';
+export { default as TourButton } from './TourButton';
+export { workspaceTourSteps, chatTourSteps } from './tourSteps';
+export type { TourStep } from './useTour';
